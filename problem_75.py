144edd04949b5108b6ccf1006ffb4248789b1aa4
Kiri11Gurin <164596083+Kiri11Gurin@users.noreply.github.com>
2024-04-28 22:20:05 +0700

    Create problem_75.py

---
+++ problem_75.py
@@ -0,0 +1,16 @@
+counter = 0
+d = {}
+for m in range(2, 870):  # 870
+    for n in range(1, m):
+        if (m - n) % 2 == 1 and mutually_prime(m, n):
+            a = m ** 2 - n ** 2
+            b = 2 * m * n
+            c = m ** 2 + n ** 2
+            k = 1
+            while (a + b + c) * k <= 1500000:
+                # print(f'a = {a * k}, a = {b * k}, a = {c * k}, P = {(a + b + c) * k}')
+                d[(a + b + c) * k] = d.get((a + b + c) * k, 0) + 1
+                k += 1
+            k = 1
+
+print(len(list(filter(lambda x: x[1] == 1, d.items()))))
